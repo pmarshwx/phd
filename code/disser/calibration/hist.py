569339ae5fca8f08c0cc1bc674803dabee26c2c8
Patrick Marsh <patrickmarshwx@gmail.com>
2012-11-17 13:19:44 -0600

    Fix how tuple of files was created

--- code/disser/calibration/hist.py
+++ code/disser/calibration/hist.py
@@ -1,6 +1,7 @@
 import hwt
 import numpy as np
 from disser import stat_tools
+import disser.misc
 
 
 def create_hist2d(kwargs):
@@ -57,38 +58,34 @@ def create_hist2d(kwargs):
     max_precip_mm = kwargs.get('max_precip_mm', 400)
     hnx = int(radius / dx)
     nx = hnx * 2 + 1
-
     # Configurations for Quantile Processing
     fcst_quantile = kwargs.get('fcst_quantile', 99.9)
     stg4_quantile = kwargs.get('stg4_quantile', 99.9)
     min_stg4_thresh = kwargs.get('min_stg4_thresh', 25.4)
     missing = kwargs.get('missing', -9999)
-
     # Configurations for Exact Amount Processing
     fcst_thresh = kwargs.get('fcst_thresh', 25.4)
     stg4_thresh = kwargs.get('stg4_thresh', 25.4)
-
     # Extract Files
     stg4_files = kwargs.get('stg4_files', None)
     fcst_files = kwargs.get('fcst_files', None)
     nout_files = kwargs.get('nout_files', None)
     mask = kwargs.get('mask', None)
-    files = (stg4_files, fcst_files, nout_files)
-
+    files = zip(stg4_files, fcst_files, nout_files)
     amts = np.arange(max_precip_mm * convert_factor + 1) / convert_factor
     amts_len = amts.shape[0]
-
     # Loop through files and create histograms
     for stg4_file, fcst_file, nout_file in files:
+        if not disser.misc.fsize_check(stg4_file): continue
+        if not disser.misc.fsize_check(fcst_file): continue
         stg4 = pygrib.open(stg4_file)[1]['values'].filled(-9999)
         fcst = pygrib.open(fcst_file)[1]['values'].filled(-9999)
-        if isinstance(mask, type(NoneType)):
+        if isinstance(mask, type(None)):
             mask = np.ones(stg4.shape, dtype='int')
         stg4_int = (stg4 * convert_factor).astype(int)
         fcst_int = (fcst * convert_factor).astype(int)
         stg4_dist, fcst_dist = hwt.bin.joint_precip(stg4_int, phd_int,
                                                     mask, amts_len)
-
         if not stg4_thresh and not fcst_thresh:
             stg4_thresh = stat_tools.quantile_to_value(
                     amts, stg4_dist, stg4_quantile)
@@ -102,12 +99,10 @@ def create_hist2d(kwargs):
                     amts, fcst_dist, stg4_quantile)
             fcst_quantile = stat_tools.value_to_quantile(
                     amts, stg4_dist, fcst_thresh)
-
         # Only compare grid points that have valid data
         # in both forecast and observations
         stg4_exceed, fcst_exceed = hwt.neighborhood.find_joint_exceed(
                 stg4, nssl, mask, stg4_thresh, fcst_thresh, missing)
-
         # Create the histogram
         hist2d = hwt.neighborhood.error_composite(
                 fcst_exceed.astype(int), stg4_exceed.astype(int), radius, dx)
