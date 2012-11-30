60373fbb2201e3fda68d396282b8120098f1f78c
Patrick Marsh <patrickmarshwx@gmail.com>
2012-11-30 00:01:19 -0600

    Refactored the value_to_quantile function

--- code/disser/stat_tools/__init__.py
+++ code/disser/stat_tools/__init__.py
@@ -49,42 +49,28 @@ def quantile_to_value(values, ecdf, quantile):
     return values[ecdf >= quantile][0]
 
 
-def value_to_quantile(values, counts, value):
+def value_to_quantile(values, ecdf, value):
     '''
-    This function takes a frequency table via the frequencies and counts as
-    separate arrays and returns the quantile based on the supplied frequency.
+    This function takes an emperical cumulative distribution function and
+    values as separate arrays and returns the quantile based on the supplied
+    value.
 
     Parameters
     ----------
     values : array_like
         The values
-    counts : array_like
+    ecdf : array_like
         Counts of each value
     value : number
         The value of the desired quantile
 
     Returns
     -------
-    quantile : float
-        The quantile of the supplied threshold
+    The quantile of the supplied threshold
 
     '''
-    values = np.asanyarray(values)
-    counts = np.asanyarray(counts)
-    value = np.float(value)
-    error = [(a - value) for a in values]
-    abs_error = np.abs(error)
-    min_abs_error = np.min(abs_error)
-    ind = np.where(abs_error == min_abs_error)[0][0]
-    error_amount = error[ind]
-    # Because we are dealing with exceedances, if the error is less than zero,
-    # it means the nearest frequency actually exceeds supplied threshold.
-    # Thus we need to take the next highest frequency, even though the error
-    # will be slightly more.
-    if error_amount < 0:
-        ind += 1
-        if ind >= values.shape[0]:
-            ind = values.shape[0]-1
-    quantile_array = counts.cumsum() / counts.sum()
-    quantile = quantile_array[ind] * 100
-    return quantile
+    ecdf = np.asarray(ecdf)
+    values = np.asarray(values)
+    return ecdf[np.where(values >= value)[0][0]]
+
+
