eee6f211300ae6b6a463f000d5051ec6c991c188
Patrick Marsh <patrickmarshwx@gmail.com>
2012-12-07 11:14:59 -0600

    Added function to create distributions

--- code/disser/calibration/hist.py
+++ code/disser/calibration/hist.py
@@ -33,7 +33,7 @@ def create_hist2d(kwargs):
             max_precip_mm : number (default 400)
                 The maximum precipitation value. All precip values
                 greater than this are considered to be this value.
-            stg4_thresh : number (default 25.4)
+                stg4_thresh : number (default 25.4)
                 The observed precipitation value being verified against.
             fcst_thresh : number (default 25.4)
                 The forecast precipitation value used as threshold
@@ -91,3 +91,69 @@ def create_hist2d(kwargs):
         np.savez_compressed(nout_file, hist2d=hist2d, stg4_thresh=stg4_thresh,
                             fcst_thresh=fcst_thresh, amts=amts, dx=dx,
                             radius=radius)
+
+
+def make_frequency_list():
+    """
+    Create a 2D histogram of where observed precipitation
+    occurred relative to forecast.
+
+    Parameters
+    ----------
+    kwargs : dict
+
+        Mandatory Keywords:
+            stg4_files : array_like
+                List of observations files
+            fcst_files : array_like
+                List of forecast files
+            nout_files : array_like
+                List of output files
+
+        Optional Keywords:
+            convert_factor : integer (default 8)
+                Number needed to multiply data by to convert
+                from decimal to integer
+            max_precip_mm : number (default 400)
+                The maximum precipitation value. All precip values
+                greater than this are considered to be this value.
+                stg4_thresh : number (default 25.4)
+                The observed precipitation value being verified against.
+            missing : number (default -9999)
+                The value of missing data in either the fcst or stg4 files.
+            mask : 2D Array
+                2D mask (1/0) with 1 being good 0 being masked
+
+    Returns
+    -------
+    None
+
+    """
+    convert_factor = kwargs.get('convert_factor', 8)
+    max_precip_mm = kwargs.get('max_precip_mm', 400)
+    missing = kwargs.get('missing', -9999)
+    # Extract Files
+    stg4_files = kwargs.get('stg4_files', None)
+    fcst_files = kwargs.get('fcst_files', None)
+    nout_files = kwargs.get('nout_files', None)
+    mask = kwargs.get('mask', None)
+    files = zip(stg4_files, fcst_files, nout_files)
+    amts = np.arange(max_precip_mm * convert_factor + 1) / convert_factor
+    amts_len = amts.shape[0]
+    # Loop through files and create histograms
+    for stg4_file, fcst_file, nout_file in files:
+        if not disser.misc.fsize_check(stg4_file): continue
+        if not disser.misc.fsize_check(fcst_file): continue
+        stg4 = pygrib.open(stg4_file)[1]['values']
+        stg4 = np.ma.asanyarray(stg4).filled(missing)
+        fcst = pygrib.open(fcst_file)[1]['values']
+        fcst = np.ma.asanyarray(fcst).filled(missing)
+        if isinstance(mask, type(None)):
+            mask = np.ones(stg4.shape, dtype='int')
+        stg4_int = (stg4 * convert_factor).astype(int)
+        fcst_int = (fcst * convert_factor).astype(int)
+        stg4_dist, fcst_dist = hwt.bin.joint_precip(stg4_int, fcst_int,
+                                                    mask, amts_len)
+        np.savez_compressed(nout_file, stg4_dist=stg4_dist,
+                            fcst_dist=fcst_dist, amts=amts)
+
