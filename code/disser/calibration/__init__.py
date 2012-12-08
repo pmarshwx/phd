b34101046edb8789df27760aceea450ba630bddc
Patrick Marsh <patrickmarshwx@gmail.com>
2012-12-07 22:08:16 -0600

    Add new function to the sub-module's init file

--- code/disser/calibration/__init__.py
+++ code/disser/calibration/__init__.py
@@ -1,4 +1,4 @@
 import hist
 import forecast
-from hist import create_hist2d, make_frequency_list
+from hist import create_hist2d, make_frequency_list, get_simulation_params
 from forecast import create_forecasts, forecast_verification
